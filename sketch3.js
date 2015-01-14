52420fb11fba8f1d3adc720668e94afbf5d93220
Noah Segal <n.h.segal@gmail.com>
2015-01-13 22:56:09 -0500

    alter the scaling of the energy bars

--- sketch3.js
+++ sketch3.js
@@ -202,7 +202,7 @@ if (nodeNum != nodeNumSlider.value()){
    initialU = calculateU();
    initialK = calculateK();
    initialAction = calculateK() - calculateU();  
-   most = max(initialAction, initialU, abs(initialK)); 
+   most = 100;//max(initialAction, initialU, abs(initialK)); 
   }
 }
 
