88ddda3d310fb2712ed092b727ad14127ad15134
Noah Segal <n.h.segal@gmail.com>
2015-02-21 23:10:11 -0500

    Normalizes energy bars and potential functions

--- sketch3.js
+++ sketch3.js
@@ -20,11 +20,12 @@ var U_;
 var reset;
 
 
+
 function setup() {
   var cnv = createCanvas(800, 400);
   cnv.parent("myContainer");
   textSize(16);
-  textFont("Georgia");
+  textFont("Consolas");
   textStyle(NORMAL);
 
   nodeNumSlider = createSlider(3,17,9);
@@ -33,32 +34,28 @@ function setup() {
   
   reset = createButton('Reset', 1);
   reset.parent("myContainer2");
-  //reset.position();
 
   reset.mousePressed(function()  {
     nodes.length = 0;
     nodeNum = nodeNumSlider.value();
-   for (var i = 0; i<nodeNum; i++) {
-    nodes.push(new Node(map(i, 0, nodeNum-1, 30, width-80), height/2));
-  }
-   initialU = calculateU();
-   initialK = calculateK();
-   initialAction = calculateK() - calculateU();  
-   most = max(initialAction, initialU, abs(initialK));  
-    });
-
+    for (var i = 0; i<nodeNum; i++) {
+      nodes.push(new Node(map(i, 0, nodeNum-1, 30, width-80), height/2));
+    }
+    initialU = calculateU();
+    initialK = calculateK();
+    initialAction = calculateK() - calculateU();  
+     most = 10000;// max(initialAction, initialU, abs(initialK));  
+  });
 
   stroke(100);
   noStroke();
   for (var i = 0; i<nodeNumSlider.value(); i++) {
     nodes.push(new Node(map(i, 0, nodeNumSlider.value()-1, 30, width-80), height/2));
   }
-
-   initialU = calculateU();
-   initialK = calculateK();
-   initialAction = calculateK() - calculateU();  
-   most = max(initialAction, initialU, abs(initialK));  
-
+  initialU = calculateU();
+  initialK = calculateK();
+  initialAction = calculateK() - calculateU();  
+  most = 10000;//max(initialAction, initialU, abs(initialK));  
 }
 
 function draw() {
@@ -96,14 +93,12 @@ function draw() {
     nodes[i].clickedOn();
     nodes[i].display();
     if (i>0) {
-        stroke(0);
-        strokeWeight(2);
-        line(nodes[i].x, nodes[i].y, nodes[i-1].x, nodes[i-1].y);
+      stroke(0);
+      strokeWeight(2);
+      line(nodes[i].x, nodes[i].y, nodes[i-1].x, nodes[i-1].y);
     }
   }
-
-  energyBars();
- 
+  energyBars(); 
 }
 
 function grid(y) {
@@ -135,7 +130,7 @@ function Node(ix, iy) {
     ellipse(this.x, this.y, 8, 8);
   }
 
- this.clickedOn = function() {
+  this.clickedOn = function() {
     if (this.selected == true) {
       this.x = mouseX;
       this.y = mouseY;
@@ -150,6 +145,7 @@ function calculateK() {
       K = K + sq(dist(nodes[i].x, nodes[i].y, nodes[i-1].x, nodes[i-1].y));
     }
   }
+  K = K*(nodeNum-1);
   return K;
 }
 
@@ -161,48 +157,45 @@ function calculateU() {
   return U;
 }
 
-
 function getPE(q) {
   switch (potential) {
-  case '1':
-      return -(200/(nodeNum)*(q.y) -380000/(nodeNum)  );
+  case '1': //linear grad upward
+    return -(2500*q.y)/nodeNum + 1500000/nodeNum;  //-(200/(nodeNum)*(q.y) -380000/(nodeNum)  
     
   case '2': 
-    return -(2000/(nodeNum*nodeNum)*(q.x) - 1900000/(nodeNum*nodeNum) );  
+    return -(2900*q.x)/nodeNum + 1500000/nodeNum;      //(20000/(nodeNum*nodeNum)*(q.x) - 1900000/(nodeNum*nodeNum) );  
       
   case '3': 
-     if (q.x + q.y > width/2 + height/2 ) {
-        return -(30000);
+    if (q.x + q.y > width/2 + height/2 ) {
+      return -(2000000/nodeNum);
       }
       else {
         return 0;
       }
-      break;
+    break;
 
   case '4': 
-    return -(300000)*(1/(dist(q.x, q.y, width/2, height/2)));
+    return -(76000000/nodeNum)*(1/(dist(q.x, q.y, width/2, height/2)));
 
   case '5':
-    return (10.0/(nodeNum*nodeNum))*(sq(q.x-width/2));
+    return (2.5/sqrt(nodeNum))*(sq(q.x-width/2));
 
   default:
     return 0;
   }
  }
 
-
-
 function numCheck(){
 if (nodeNum != nodeNumSlider.value()){
- nodes.length = 0;
-    nodeNum = nodeNumSlider.value();
-   for (var i = 0; i<nodeNum; i++) {
+  nodes.length = 0;
+  nodeNum = nodeNumSlider.value();
+  for (var i = 0; i<nodeNum; i++) {
     nodes.push(new Node(map(i, 0, nodeNum-1, 30, width-80), height/2));
   }
-   initialU = calculateU();
-   initialK = calculateK();
-   initialAction = calculateK() - calculateU();  
-   most = 100000;//max(initialAction, initialU, abs(initialK)); 
+  initialU = calculateU();
+  initialK = calculateK();
+  initialAction = calculateK() - calculateU();  
+  most = 10000; // max(initialAction, initialU, abs(initialK)); 
   }
 }
 
@@ -227,10 +220,10 @@ function mouseClicked() {
    for (var i = 0; i<nodeNum; i++) {
     nodes.push(new Node(map(i, 0, nodeNum-1, 30, width-80), height/2));
   }
-   initialU = calculateU();
-   initialK = calculateK();
-   initialAction = calculateK() - calculateU();  
-    most = 100000;//max(initialAction, initialU, abs(initialK)); 
+  initialU = calculateU();
+  initialK = calculateK();
+  initialAction = calculateK() - calculateU();  
+  most = 10000; //max(initialAction, initialU, abs(initialK)); 
   }
 }
 
@@ -255,33 +248,45 @@ function optimizer() {
 }
 
 function energyBars(){
-    stroke(200, 0, 0);
-    fill(200, 0, 0);
-    var K_ = calculateK();
-    var U_ = calculateU();
+  stroke(200, 0, 0);
+  fill(200, 0, 0);
+  var K_ = calculateK();
+  var U_ = calculateU();
 
-    rect(width-60, 2*height/3, 10, -(K_/most)*100);
+  rect(width-60, 2*height/3, 10, -(K_/most));
     
-    stroke(0, 200, 0);
-    fill(0, 200, 0);
-    rect(width-40, 2*height/3, 10, -(U_/most)*100);
-    fill(0);
+  stroke(0, 200, 0);
+  fill(0, 200, 0);
+  rect(width-40, 2*height/3, 10, -(U_/most));
+  fill(0);
    
-    stroke(0, 0, 200);
-    fill(0, 0, 200);
+  stroke(0, 0, 200);
+  fill(0, 0, 200);
 
-    rect(width-20, 2*height/3, 10, (-(K_-U_)/most)*100);
-    fill(0);
-    stroke(0,0,0);
-    strokeWeight(0.2);
-    text("K", width-60+2, height/6-10);
-    text("U", width-40+2, height/6-10);
-    text("S", width-20+2, height/6-10);
-    text("Action = " + String(Math.round( calculateK()/100-calculateU()/100 )), width/2, 17);
+  rect(width-20, 2*height/3, 10, (-(K_-U_)/most));
+  fill(0);
+  stroke(0,0,0);
+  strokeWeight(0.2);
+  textAlign(LEFT);
+  text("K", width-60+2, height/6-10);
+  text("U", width-40+2, height/6-10);
+  text("S", width-20+2, height/6-10);
+  text("Kinetic   = ", width/2, 17);
+
+  text("Potential = ", width/2, 34);
+  text("Action    =  ", width/2, 51);
+
+  textAlign(RIGHT);
+  text(String(Math.round( calculateK()/100 )), width/2 + 155, 17);
+  text(String(Math.round( calculateU()/100 )), width/2 + 155, 34);
+  text(String(Math.round( calculateK()/100 - calculateU()/100 )), width/2 + 155, 51);
+
+
+
+
 }
-
 
 function myFunction() {
   var e = document.getElementById("menu1");
   potential = e.options[e.selectedIndex].value;
-  }
+}
